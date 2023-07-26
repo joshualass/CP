f14eca9ceb17d8c982b7672bc55baea6f421bb2c
joshualass <111375438+joshualass@users.noreply.github.com>
2023-07-26 09:51:48 -0500

    Update binarytrie.cpp

--- Templates/Data Structures/Tries/binarytrie.cpp
+++ Templates/Data Structures/Tries/binarytrie.cpp
@@ -1,1 +1,32 @@
+/*
+binary trie which stores bits
+useful for solving maximum xor problems
+*/
+const int depth = 64;
 
+struct TrieNode {
+    int value;
+    TrieNode *arr[2];
+};
+
+TrieNode *newNode() {
+    TrieNode *temp = new TrieNode;
+    temp->value = 0;
+    temp->arr[0] = nullptr;
+    temp->arr[1] = nullptr;
+    return temp;
+}
+
+void insertNode(int num, TrieNode* root) {
+    TrieNode* curr = root;
+    for(int i = 0; i < depth; i++) {
+        int idx = (num >> (depth - i - 1)) & 1;
+        if(curr->arr[idx]) {
+            curr = curr->arr[idx];
+        } else {
+            curr->arr[idx] = newNode();
+            curr = curr->arr[idx];
+        }
+    }
+    curr->value = num;
+}
