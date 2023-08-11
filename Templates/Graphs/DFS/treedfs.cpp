4904d32e3510f5ef649b893f91d2e66169f99c52
joshualass <111375438+joshualass@users.noreply.github.com>
2023-08-11 14:36:15 -0500

    Update treedfs.cpp

--- Templates/Graphs/DFS/treedfs.cpp
+++ Templates/Graphs/DFS/treedfs.cpp
@@ -3,7 +3,7 @@ void dfs(int v, int par, int h, vector<int> &d, vector<vector<int>> &adj) {
     d[v] = h;
     for (int i : adj[v]) {
         if (i != par) {
-            dfs(i, v, h + 1, d);
+            dfs(i, v, h + 1, d, adj);
         }
     }
 }
