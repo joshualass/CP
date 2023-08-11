16bca62c13a13cde1d85c7163885a18a034c5907
joshualass <111375438+joshualass@users.noreply.github.com>
2023-08-11 13:25:36 -0500

    Create treedfs.cpp

---
+++ Templates/Graphs/DFS/treedfs.cpp
@@ -0,0 +1,8 @@
+void dfs(int v, int par, int h, vector<int> &d, vector<vector<int>> &adj) {
+    d[v] = h;
+    for (int i : adj[v]) {
+        if (i != par) {
+            dfs(i, v, h + 1, d);
+        }
+    }
+}
