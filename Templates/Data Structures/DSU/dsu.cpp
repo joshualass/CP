f06dc146c8f35f52d521d7f2662402b923660f30
joshualass <111375438+joshualass@users.noreply.github.com>
2023-08-19 18:17:23 -0500

    Create dsu.cpp

---
+++ Templates/Data Structures/DSU/dsu.cpp
@@ -0,0 +1,31 @@
+const int N = 200000;
+int dsu[N];
+int depths[N];
+int sizes[N];
+
+int find(int x) {
+    if(dsu[x] == x) {
+        return x;
+    }
+    dsu[x] = find(dsu[x]);
+    return dsu[x];
+}
+
+void merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if(x == y) {
+        return;
+    }
+    if(sizes[x] == sizes[y]) {
+        dsu[y] = x;
+        depths[x]++;
+        sizes[x] += sizes[y];
+    } else if(sizes[x] < sizes[y]) {
+        dsu[y] = x;
+        sizes[x] += sizes[y];
+    } else {
+        dsu[x] = y;
+        sizes[y] += sizes[x];
+    }
+}
