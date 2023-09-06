72fc0257cc7337744bd19b744df40080ef4f36af
joshualass <111375438+joshualass@users.noreply.github.com>
2023-09-06 12:04:16 -0500

    Update sieve.cpp

--- Templates/Math/sieve.cpp
+++ Templates/Math/sieve.cpp
@@ -1,5 +1,7 @@
 //https://codeforces.com/blog/entry/54090
 //linear sieve
+
+//Version 1 - simply stores primes and is_composite
 std::vector <int> prime;
 bool is_composite[MAXN];
 
@@ -9,6 +11,26 @@ void sieve (int n) {
 		if (!is_composite[i]) prime.push_back (i);
 		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
 			is_composite[i * prime[j]] = true;
+			if (i % prime[j] == 0) break;
+		}
+	}
+}
+
+//Version 2 - Version 1, but if a number isn't prime, stores one of its factors
+
+const int N = 1e5 + 1;
+
+std::vector <int> prime;
+int prime_factor[N];
+
+void sieve (int n) {
+	std::fill (prime_factor, prime_factor + n, -1);
+	for (int i = 2; i < n; ++i) {
+		if (prime_factor[i] == -1) {
+            prime.push_back (i);
+        }
+		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
+			prime_factor[i * prime[j]] = prime[j];
 			if (i % prime[j] == 0) break;
 		}
 	}
