29190eee56e44f7015a621561e578ed428f1749d
joshualass <111375438+joshualass@users.noreply.github.com>
2023-09-08 10:46:06 -0500

    Update sieve.cpp

--- Templates/Math/sieve.cpp
+++ Templates/Math/sieve.cpp
@@ -1,33 +1,14 @@
-//https://codeforces.com/blog/entry/54090
-//linear sieve
+const int N = 1e6 + 1;
 
-//Version 1 - simply stores primes and is_composite
-std::vector <int> prime;
-bool is_composite[MAXN];
-
-void sieve (int n) {
-	std::fill (is_composite, is_composite + n, false);
-	for (int i = 2; i < n; ++i) {
-		if (!is_composite[i]) prime.push_back (i);
-		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
-			is_composite[i * prime[j]] = true;
-			if (i % prime[j] == 0) break;
-		}
-	}
-}
-
-//Version 2 - Version 1, but if a number isn't prime, stores one of its factors
-
-const int N = 1e5 + 1;
-
-std::vector <int> prime;
-int prime_factor[N];
+int prime_factor[N]; //stores a prime factor of the number. If it is prime, stores itself.
+vector <int> prime;
 
 void sieve (int n) {
 	std::fill (prime_factor, prime_factor + n, -1);
 	for (int i = 2; i < n; ++i) {
 		if (prime_factor[i] == -1) {
-            prime.push_back (i);
+		    prime.push_back(i);
+            prime_factor[i] = i;
         }
 		for (int j = 0; j < prime.size () && i * prime[j] < n; ++j) {
 			prime_factor[i * prime[j]] = prime[j];
