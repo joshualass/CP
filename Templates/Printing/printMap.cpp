b41fdf4885fae0251b5a2edd7baf2ee22ec6e098
joshualass <111375438+joshualass@users.noreply.github.com>
2023-10-04 22:36:16 -0500

    add lazysegtree template + print fix
    courtesy of andwerp

---
+++ Templates/Printing/printMap.cpp
@@ -0,0 +1,5 @@
+template<typename T, typename D>
+std::ostream& operator<<(std::ostream& os, map<T,D> m) {
+    for(auto &x: m) os << x.first << " " << x.second << " | ";
+    return os;
+}
--- Templates/Printing/printUnorderedMap.cpp
+++ Templates/Printing/printUnorderedMap.cpp
@@ -1,5 +1,5 @@
 template<typename T, typename D>
 std::ostream& operator<<(std::ostream& os, unordered_map<T,D> m) {
-    for(auto &x: m) cout << x.first << " " << x.second << "| ";
+    for(auto &x: m) os << x.first << " " << x.second << "| ";
     return os;
 }
