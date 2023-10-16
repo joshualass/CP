41a188c88f56571ea95d1eb83baad04287c99c55
joshualass <111375438+joshualass@users.noreply.github.com>
2023-10-16 01:35:17 -0500

    tenzingballs

--- Solutions/Default/tenzingballs.cpp
+++ Solutions/Default/tenzingballs.cpp
@@ -3,8 +3,18 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
+//just become better at doing dp
+
 void solve() {
-    
+    int n; cin >> n;
+    vector<int> dp(n+1);
+    vector<int> best_before_val(n+1,1e9);
+    for(int i = 1; i <= n; i++) {
+        int num; cin >> num;
+        dp[i] = min(dp[i-1] + 1, best_before_val[num]);
+        best_before_val[num] = min(best_before_val[num],dp[i-1]);
+    }
+    cout << n - dp[n] << "\n";
 }
 
 int main() {
