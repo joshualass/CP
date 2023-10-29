06d8929d50bc7d364e132005fb652f04dd905813
joshualass <111375438+joshualass@users.noreply.github.com>
2023-10-29 16:24:16 -0500

    beef up point template

--- Templates/Geometry/point.cpp
+++ Templates/Geometry/point.cpp
@@ -1,4 +1,46 @@
+template<class F>
 struct Point {
-    ld x;
-    ld y;
+    F x, y;
+    Point(): x(0), y(0) {}
+    Point(const F& x, const F& y): x(x), y(y) {}
+
+    void swap(Point& other) {
+        swap(x, other.x);
+        swap(y, other.y);
+    }
+
+    template <class F1> explicit operator Point<F1> () {
+        return Point<F1>(static_cast<F1>(x), static_cast<F1>(y));
+    }
+
+    template <class F1> Point& operator=(const Point<F1>& other) {
+        x = other.x;
+        y = other.y;
+        return *this;
+    }
+
+    template <class F1> Point& operator +=(const Point<F1>& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    template<class F1> Point& operator -=(const Point<F1>& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    template<class F1> Point& operator *=(const F1& factor) {
+        x *= factor;
+        y *= factor;
+        return *this;
+    }
+
+    template<class F1> Point& operator /= (const F1& factor) {
+        x /= factor;
+        y /= factor;
+        return *this;
+    }
+
 };
