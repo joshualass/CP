9e9b9580c4f3cb98611de3600f0c60c3fc6ec87a
joshualass <111375438+joshualass@users.noreply.github.com>
2023-10-30 12:31:06 -0500

    al cash line template

--- Templates/Geometry/line.cpp
+++ Templates/Geometry/line.cpp
@@ -65,3 +65,119 @@ bool onSegment(const Point<F1>& point, const Line<F2>& seg) {
     }
 }
 
+template<class F1, class F2> using distF = decltype(sqrt(F1() + F2()));
+
+template <class F1, class F2>
+distF<F1, F2> distLine(const Point<F1>& point, const Line<F2>& line) {
+    if (!line) {
+        return dist(point, line.a);
+    } else {
+        return abs((point - line.a) ^ line.ab) / norm(line.ab);
+    }
+}
+
+template <class F1, class F2>
+distF<F1, F2> distSegment(const Point<F1>& point, const Line<F2>& seg) {
+    if(((point - seg.a) * seg.ab) <= 0) {
+        return dist(point, seg.a);
+    } else if(((point - seg.b()) * seg.ab) >= 0) {
+        return dist(point, seg.b());
+    } else {
+        return distLine(point,seg);
+    }
+}
+
+template <class F1, class F2, class F3>
+void projection(const Point<F1>& point, const Line<F2>& line, Point<F3>& res) {
+  res = line.a;
+  if (line) res += line.ab * static_cast<F3>((point - line.a) * line.ab) / abs(line.ab);
+}
+
+template <class F1, class F2, class F3>
+void reflection(const Point<F1>& point, const Line<F2>& line, Point<F3>& res) {
+  projection(point, line, res);
+  res = 2 * res - point;
+}
+
+template <class F1, class F2, class F3>
+void closest(const Point<F1>& point, const Line<F2>& seg, Point<F3>& res) {
+  if (((point - seg.a) * seg.ab) <= 0) res = seg.a;
+  else if (((point - seg.b()) * seg.ab) >= 0) res = seg.b();
+  else projection(point, seg, res);
+}
+
+template <int TYPE> struct EndpointChecker {};
+
+template <> struct EndpointChecker<0> {  // no endpoint (ray)
+  template <class F> bool operator ()(const F& a, const F& b) const { 
+    return true; 
+    }
+};
+
+template <> struct EndpointChecker<1> {  // closed endpoint
+  template <class F> bool operator ()(const F& a, const F& b) const { 
+    return a <= b; 
+    }
+};
+
+template <> struct EndpointChecker<2> {  // open endpoint
+  template <class F> bool operator ()(const F& a, const F& b) const { 
+    return a < b; 
+    }
+};
+
+template <int LA, int LB, int RA, int RB, class F1, class F2, class F3>
+bool intersectLines(const Line<F1>& lhs, const Line<F2>& rhs, Point<F3>& res) {
+
+  assert(lhs && rhs);
+  auto s = lhs.ab ^ rhs.ab;
+
+  if (s == 0) return false;
+
+  auto ls = (rhs.a - lhs.a) ^ rhs.ab;
+  auto rs = (rhs.a - lhs.a) ^ lhs.ab;
+
+  if (s < 0) s = -s, ls = -ls, rs = -rs;
+
+  bool intersect =
+    EndpointChecker<LA>()(decltype(ls)(0), ls) && EndpointChecker<LB>()(ls, s) &&
+    EndpointChecker<RA>()(decltype(rs)(0), rs) && EndpointChecker<RB>()(rs, s);
+
+  if (intersect) res = lhs.a + lhs.ab * static_cast<F3>(ls) / s;
+  return intersect;
+}
+
+template <class F1, class F2, class F3>
+bool intersectClosedSegments(const Line<F1>& lhs, const Line<F2>& rhs, Point<F3>& res) {
+  return intersectLines<1, 1, 1, 1>(lhs, rhs, res);
+}
+
+template <class F1, class F2, class F3>
+bool intersectSegments(const Line<F1>& lhs, const Line<F2>& rhs, Line<F3>& res) {
+  auto s = lhs.ab ^ rhs.ab;
+  auto ls = (rhs.a - lhs.a) ^ rhs.ab;
+
+  if (s == 0) {
+    if (ls != 0) return false;
+
+    auto lhsa = lhs.a, lhsb = lhs.b();
+    auto rhsa = rhs.a, rhsb = rhs.b();
+
+    if (lhsa > lhsb) swap(lhsa, lhsb);
+    if (rhsa > rhsb) swap(rhsa, rhsb);
+
+    res = Line<F3>(max(lhsa, rhsa), min(lhsb, rhsb));
+
+    return res.ab >= Point<F3>();
+  }
+  auto rs = (rhs.a - lhs.a) ^ lhs.ab;
+
+  if (s < 0) s = -s, ls = -ls, rs = -rs;
+
+  bool intersect = 0 <= ls && ls <= s && 0 <= rs && rs <= s;
+
+  if (intersect)
+    res = Line<F3>(lhs.a + lhs.ab * static_cast<F3>(ls) / s, Point<F3>());
+    
+  return intersect;
+}
