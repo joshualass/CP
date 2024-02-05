5cf43579203569b205e9086a275e1175f949f53d
joshualass <111375438+joshualass@users.noreply.github.com>
2024-02-05 03:05:24 -0600

    cses rq solves

--- Templates/Data Structures/Trees/segtree.cpp
+++ Templates/Data Structures/Trees/segtree.cpp
@@ -1,23 +1,67 @@
 template<typename T>
 struct Tree {
     static constexpr T base = 0;
-    T f(T a, T b) {
-        return a + b; //update this function for different types of queries, currently sum
+    vector<T> v;
+    int n, size;
+    T comb(T a, T b) { //change this when doing maximum vs minimum etc.
+        return max(a,b); 
     }
-    vector<T> s; int n;
-    Tree(int n = 0, T def = base) : s(n*2,def), n(n) {}
-    void build() {
-        for(int i = n - 1; i > 0; i--) s[i] = f(s[i*2],s[i*2+1]);
+    // optional MIQ stuff
+    bool cond(T a, T b) {
+        return a <= b;
     }
-    void update(int pos, T val) {
-        for(s[pos += n] = val; pos /= 2;) s[pos] = f(s[pos*2],s[pos*2+1]);
+    Tree(int n = 0, T def = base) {
+        this->n = n; //max number of elements
+        size = 1;
+        while(size < n) size *= 2;
+        v.assign(size * 2, def);
     }
-    T query(int l, int r) { //[l,r)
-        T ans = base;
-        for(l += n, r += n; l < r; l /= 2, r /= 2) {
-            if(l & 1) ans = f(ans, s[l++]);
-            if(r & 1) ans = f(s[--r],ans);
+    void update(int pos, T val) { //update 0 indexed, assignment
+        assert(pos < n && pos >= 0);
+        int curr = pos + size;
+        v[curr] = val;
+        while(curr != 1) {
+            if(curr & 1) { //sometimes associative
+                comb(v[curr ^ 1], v[curr]);
+            } else {
+                comb(v[curr], v[curr ^ 1]);
+            }
+            curr /= 2;
         }
+    }
+    bool isLeaf(int idx) {
+        return idx >= size;
+    }
+    T at(int idx) {
+        assert(idx >= 0 && idx < n);
+        return v[idx + size];
+    }
+    T query(int l, int r) {//queries in range [l,r)
+        return _query(1,0,size,l,r);
+    }
+    T _query(int idx, int currl, int currr, int &targetl, int &targetr) {
+        if(currr <= targetl || currl >= targetr) return base;
+        if(currl >= targetl && currr <= targetr) return v[idx];
+        int mid = (currl + currr) / 2;
+        return comb(
+            _query(idx * 2, currl, mid, targetl, targetr),
+            _query(idx * 2 + 1, mid, currr, targetl, targetr)
+        );
+    }
+    //find the lowest index and value that satisfy the condition in range [l,r)
+    pair<int,T> queryMIQ(T val, int lo, int hi) { //{index, value}
+        pair<int,T> ans = _queryMIQ(1, 0, size, lo, hi, val);
         return ans;
+    }   
+    pair<int,T> _queryMIQ(int idx, int currl, int currr, int &lo, int &hi, T &val) {
+        //check overlap of left bound
+        if(cond(val, v[idx])) {
+            if(isLeaf(idx)) return {idx - size, v[idx]};
+            int mid = (currl + currr) / 2;
+            if(mid > lo && cond(val, v[idx * 2])) return _queryMIQ(idx * 2, currl, mid, lo, hi, val);
+            return _queryMIQ(idx * 2 + 1, mid, currr, lo, hi, val);
+        } else {
+            return {-1,base};
+        }
     }
 };
