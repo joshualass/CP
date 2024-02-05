052229703ab1d8f0ca2cb4f14a12cbf256a3616c
joshualass <111375438+joshualass@users.noreply.github.com>
2024-02-04 19:37:34 -0600

    lazy segtree optimization

--- Templates/Data Structures/Trees/lazysegtree.cpp
+++ Templates/Data Structures/Trees/lazysegtree.cpp
@@ -1,19 +1,18 @@
-template<typename T>
+template<typename T, typename D>
 struct Lazy {
-    static constexpr T ln = 0, qn = 0; //stores the starting values at all nodes, 
+    static constexpr T qn = 0; //stores the starting values at all nodes, 
+    static constexpr D ln = 0;
     vector<T> v;      //stores values at each index we are querying for
-    vector<T> lazy;   //stores lazy updates at each node
-    vector<bool> upd; //stores at each node, whether there is a lazy update to push
+    vector<D> lazy;   //base, count of how many polynomials start at one at the beginning of this node
     int n, size;
 
     //if OJ is not up to date, remove all occurrences of ln
-    Lazy(int n = 0, T def = ln) {
+    Lazy(int n = 0, T def = qn) {
         this->n = n;
         this->size = 1;
         while(size < n) size *= 2;
         v.assign(size * 2, def);
         lazy.assign(size * 2, ln);
-        upd.assign(size * 2, 0);
     }
     bool isLeaf(int node) {
         return node >= size;
@@ -22,41 +21,34 @@ struct Lazy {
         return val1 + val2;
     }
     //how we combine lazy updates to lazy
-    void lazy_comb(int node, T val) {//update this depending on update type. how do we merge the lazy changes?
-        lazy[node] = lazy[node] + val; 
-        upd[node] = 1;
+    void lazy_comb(int node, D val) {//update this depending on update type. how do we merge the lazy changes?
+        lazy[node] += val;
     }
     void main_comb(int node, int size) {//update this depending on query type, how does the lazy value affect value at v for the query?
-        v[node] += lazy[node] * size;
+        v[node] += lazy[node];
     }
     void push_lazy(int node, int size) {
         main_comb(node, size); //push lazy change to current node
         if(!isLeaf(node)) {
             lazy_comb(node * 2, lazy[node]);
             lazy_comb(node * 2 + 1, lazy[node]);
-            upd[node * 2] = 1;
-            upd[node * 2 + 1] = 1;
         }
         lazy[node] = ln;
-        upd[node] = 0;
     }
-    void update(int l, int r, T val) {
-        _update(1,0,size,l,r,val);
+    void update(int l, int r, D val) {
+        _update(1,0,size,l,r, val);
     }
-    void _update(int node, int currl, int currr, int &targetl, int &targetr, T &val) {
-        if(upd[node]) {
-            push_lazy(node,currr-currl);
-        }
-        if (currl >= targetr || currr <= targetl) { //there is no overlap
-            return; //do nothing if there is no overlap
-        }
+    void _update(int node, int currl, int currr, int &targetl, int &targetr, D val) {
+        push_lazy(node, currr - currl);
+        if (currl >= targetr || currr <= targetl) return;
         if(currl >= targetl && currr <= targetr) { //complete overlap
             lazy_comb(node, val); //we apply the lazy change to this node, then update this node.
-            push_lazy(node, currr-currl);
         } else { //partial overlap, should never be a leaf, otherwise it'd fall under previous categories 
             int mid = (currl + currr) / 2;
             _update(node * 2, currl, mid, targetl, targetr, val);
             _update(node * 2 + 1, mid, currr, targetl, targetr, val);
+            push_lazy(node * 2, (currr - currl) / 2);
+            push_lazy(node * 2 + 1, (currr - currl) / 2);
             v[node] = query_comb(v[node * 2], v[node * 2 + 1]);
         }
     }
@@ -64,10 +56,8 @@ struct Lazy {
         return _query(1,0,size,l,r);
     }
     T _query(int node, int currl, int currr, int &targetl, int &targetr) { //[l,r)
-        if(currr <= targetl || currl >= targetr) { //no overlap
-            return qn; 
-        } 
-        if(upd[node]) push_lazy(node, currr-currl); //make pushes necessary before getting value, we always check for 2 cases
+        if(currr <= targetl || currl >= targetr)return qn; 
+        push_lazy(node, currr-currl); //make pushes necessary before getting value, we always check for 2 cases
         if(currl >= targetl && currr <= targetr) { //complete overlap
             return v[node];
         } else {
