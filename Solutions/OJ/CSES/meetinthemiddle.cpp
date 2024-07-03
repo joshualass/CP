dfc8f049e85fede28795dedaaeae50e09b5c36f7
joshualass <111375438+joshualass@users.noreply.github.com>
2024-07-03 11:14:25 -0500

    Update meetinthemiddle.cpp

--- Solutions/OJ/CSES/meetinthemiddle.cpp
+++ Solutions/OJ/CSES/meetinthemiddle.cpp
@@ -1,46 +1,59 @@
 #include <bits/stdc++.h>
 typedef long long ll;
+typedef long double ld;
 using namespace std;
-
-int main() {
+const ll MOD = 1e9 + 7;
+ 
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+struct chash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+template<typename T> using pb_set = gp_hash_table<T, null_type, chash>;  // unordered_set but faster
+template<typename T, typename U> using pb_map = gp_hash_table<T, U, chash>; // unordered_map but faster
+ 
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
     ll n, x; cin >> n >> x;
-    unordered_map<ll,ll> m;
-    vector<ll> v(n);
-    for(ll &x: v) cin >> x;
-    ll first = n / 2;
-    ll second = n - first;
-    for(ll i = 0; i < (1 << first); i++) {
-        ll num = i;
+    vector<ll> a((n+1) / 2);
+    vector<ll> b(n / 2);
+    for(ll &x : a) cin >> x;
+    for(ll &x : b) cin >> x;
+    
+    pb_map<ll,ll> m;
+    for(int bm = 0; bm < 1 << b.size(); bm++) {
         ll cnt = 0;
-        for(ll j = 0; j < first; j++) {
-            if((num >> j) & 1) {
-                cnt += v[j];
+        for(int i = 0; i < b.size(); i++) {
+            if((bm >> i) & 1) {
+                cnt += b[i];
             }
         }
-        if(m.find(cnt) == m.end()) {
-            m[cnt] = 1;
-        } else {
-            m[cnt]++;
+        m[cnt]++;
+    }
+    
+    ll res = 0;
+    for(int bm = 0; bm < 1 << a.size(); bm++) {
+        ll c = 0;
+        for(int i = 0; i < a.size(); i++) {
+            if((bm >> i) & 1) c += a[i];
+        }
+        if(m.find(x-c) != m.end()) {
+            res += m[x-c];
         }
     }
-
-    v.erase(v.begin(), v.begin() + first);
-    ll ans =0;
-    for(ll i = 0; i < (1 << second); i++) {
-        ll num = i;
-        ll cnt = 0;
-        for(ll j = 0; j < second; j++) {
-            if((num >> j) & 1) {
-                cnt += v[j];
-            }
-        }
-        if(m.find(x-cnt) != m.end()) {
-            ans += m[x-cnt];
-        }
-    }
-    cout << ans << "\n";
+    cout << res << '\n';
     return 0;
 }
