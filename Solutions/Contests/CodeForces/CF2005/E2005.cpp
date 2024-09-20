02fe183ec84a122b3e590207098a70d1b44a43cc
lassjoshua <joshua.lass@tamu.edu>
2024-09-20 16:12:01 -0500

    recent solves

--- Solutions/Contests/CodeForces/CF2005/E2005.cpp
+++ Solutions/Contests/CodeForces/CF2005/E2005.cpp
@@ -4,67 +4,101 @@ typedef long double ld;
 using namespace std;
 const ll MOD = 998244353;
 
+//consider taking a trip to implementation school
+
+/*
+many wa bc forgot definition of the problem
+
+make the observation that we should always choose later a[i] because it gives less freedom. 
+In other words, we shouldn't choose an element if there is the same element which is in its bottom-right rectangle.
+
+*/
+
 void solve() {
     
     int l, n, m; cin >> l >> n >> m;
-    map<int,int> ids;
-
-    for(int i = 0; i < n; i++) {
-        int num; cin >> num;
-        if(ids.count(num) == 0) {
-            ids[num] = i;
+    vector<int> ids(n*m,-1);
+    for(int i = 0; i < l; i++) {
+        int a; cin >> a;
+        a--;
+        if(ids[a] == -1) {
+            ids[a] = i;
         }
     }
 
-    vector<vector<set<int>>> nears(l, vector<set<int>>(n));
+    //for element i in array a, the max col it appears for each row. 
+    vector<vector<int>> hicols(l, vector<int>(n,-1));
 
+    //get hi cols
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             int num; cin >> num;
-            if(ids.count(num)) {
-                nears[ids[num]][i].insert(j);
+            num--;
+            if(ids[num] != -1) {
+                hicols[ids[num]][i] = j;
             }
         }
     }
 
-    vector<vector<int>> r(l, vector<int>(n,-2));
-
+    //prune hi cols, so that they make a mono set
     for(int i = 0; i < l; i++) {
-
-        //cur is current column of previous mono set
-        int cur = -2;
-        if(i = 0) {
-            cur = -1;
-        }
-
-        //hi is current highest index of current mono set
-        int hi = -2;
-
+        int hicol = -1;
         for(int j = n - 1; j >= 0; j--) {
-
-            if(nears[l][j].size()) {
-                int highest = (*--nears[l][j].lower_bound(m));
-                if()
+            if(hicols[i][j] > hicol) {
+                hicol = hicols[i][j];
+            } else {
+                hicols[i][j] = -1;
             }
-
         }
-
-
-        for(int j = 0; j < n; j++) {
-
-            if(cur != -2 && nears[i][j].upper_bound(cur) != nears[i][j].end()) {
-
-            }
-
-
-
-            if(i && r[i-1][j] != -2) {
-                cur = r[i-1][j];
-            }
-
-        }
-
     }
+
+    //given a previous mono set, ensure the next one is nested.
+    for(int i = 1; i < l; i++) {
+        int locol = m;
+        for(int j = 0; j < n; j++) {
+            //handle case when directly below which is ok. 
+            //has to be completely nested. 
+            if(hicols[i][j] <= locol) {
+                hicols[i][j] = -1;
+            }
+            if(hicols[i-1][j] != -1) {
+                locol = hicols[i-1][j];
+            }
+
+            // int keep = 0;
+            // if(hicols[i][j] >= locol) {
+            //     keep = 1;
+            // }
+            // if(hicols[i-1][j] != -1) {
+            //     locol = hicols[i-1][j];
+            // }
+            // if(hicols[i][j] <= locol && !keep) {
+            //     hicols[i][j] = -1;
+            // }
+        }
+    }
+
+    //find winners
+    for(int i = l - 2; i >= 0; i--) {
+        int hicol = -1;
+        for(int j = n - 1; j >= 0; j--) {
+            if(hicol > hicols[i][j]) {
+                hicols[i][j] = -1;
+            }
+            if(hicols[i+1][j] != -1) {
+                hicol = hicols[i+1][j];
+            }
+        }
+    }
+
+    int found = 0;
+    for(int i = 0; i < n; i++) {
+        if(hicols[0][i] != -1) {
+            found = 1;
+        }
+    }
+
+    cout << (found ? 'T' : 'N') << '\n';
 
 }
 
@@ -77,3 +111,83 @@ signed main() {
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// typedef long long ll;
+// typedef long double ld;
+// using namespace std;
+// const ll MOD = 998244353;
+
+// void solve() {
+    
+//     int l, n, m; cin >> l >> n >> m;
+//     map<int,int> ids;
+
+//     for(int i = 0; i < n; i++) {
+//         int num; cin >> num;
+//         if(ids.count(num) == 0) {
+//             ids[num] = i;
+//         }
+//     }
+
+//     vector<vector<set<int>>> nears(l, vector<set<int>>(n));
+
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < m; j++) {
+//             int num; cin >> num;
+//             if(ids.count(num)) {
+//                 nears[ids[num]][i].insert(j);
+//             }
+//         }
+//     }
+
+//     vector<vector<int>> r(l, vector<int>(n,-2));
+
+//     for(int i = 0; i < l; i++) {
+
+//         //cur is current column of previous mono set
+//         int cur = -2;
+//         if(i = 0) {
+//             cur = -1;
+//         }
+
+//         //hi is current highest index of current mono set
+//         int hi = -2;
+
+//         for(int j = n - 1; j >= 0; j--) {
+
+//             if(nears[l][j].size()) {
+//                 int highest = (*--nears[l][j].lower_bound(m));
+//                 if()
+//             }
+
+//         }
+
+
+//         for(int j = 0; j < n; j++) {
+
+//             if(cur != -2 && nears[i][j].upper_bound(cur) != nears[i][j].end()) {
+
+//             }
+
+
+
+//             if(i && r[i-1][j] != -2) {
+//                 cur = r[i-1][j];
+//             }
+
+//         }
+
+//     }
+
+// }
+
+// signed main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int casi; cin >> casi;
+//     while(casi-->0) solve();
+
+//     return 0;
+// }
--- Templates/Beta/test2.cpp
+++ Templates/Beta/test2.cpp
@@ -1,25 +1,134 @@
+// #include <bits/stdc++.h>
+// typedef long long ll;
+// typedef long double ld;
+// using namespace std;
+// const ll MOD = 1e9 + 7;
+
+// template<typename T>
+// std::ostream& operator<<(std::ostream& os, const vector<T> v) {
+//     for(auto x : v) os << x << " ";
+//     return os;
+// }
+
+// int cnt_cycles(vector<int> &a) {
+//     int cnt = 0;
+//     vector<int> vis(a.size());
+
+//     for(int i = 0; i < a.size(); i++) {
+//         if(vis[i] == 0) {
+//             queue<int> q;
+//             q.push(i);
+//             while(q.size()) {
+//                 int b = q.front();
+//                 q.pop();
+//                 if(vis[b]) continue;
+//                 vis[b] = 1;
+//                 q.push(a[b]);
+//             }
+//             cnt++;
+//         }
+//     }
+//     // cout << "a : " << a << " cnt : " << cnt << '\n';
+//     return cnt;
+// }
+
+// signed main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     cout << "gimme n : " << endl;
+//     int n; cin >> n;
+//     vector<int> a(n);
+//     iota(a.begin(), a.end(), 0);
+//     ll cycles = n, cnt = 1;
+//     while(next_permutation(a.begin(), a.end())) {
+//         cnt++;
+//         cycles += cnt_cycles(a);
+//     }
+
+//     cout << "cnt : " << cnt << " cycles : " << cycles << " expected : " << (((ld) cycles) / cnt) << '\n';
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 typedef long long ll;
 typedef long double ld;
 using namespace std;
 const ll MOD = 1e9 + 7;
 
+ll gcdExtended(ll a, ll b, ll *x, ll *y);
+ 
+ll modInverse(ll b, ll m) {
+    ll x, y;
+    ll g = gcdExtended(b, m, &x, &y);
+ 
+    if (g != 1) return -1;
+ 
+    return (x % m + m) % m;
+}
+  // Function to compute a/b under modulo m
+ll modDivide(ll a, ll b) {
+    a = a % MOD;
+    int inv = modInverse(b, MOD);
+    return (inv * a) % MOD;
+}
+ 
+ll gcdExtended(ll a, ll b, ll *x, ll *y) {
+    if (a == 0) {
+        *x = 0, *y = 1;
+        return b;
+    }
+ 
+    ll x1, y1;
+    ll gcd = gcdExtended(b % a, a, &x1, &y1);
+ 
+    *x = y1 - (b / a) * x1;
+    *y = x1;
+ 
+    return gcd;
+}
+
+ll binexp(ll base, ll power) {
+    ll ans = 1;
+    while(power != 0) {
+        if(power & 1) {
+            ans = ans*base%MOD;
+        }
+        base = base*base%MOD;
+        power >>= 1;
+    }
+    return ans;
+}
+
+vector<ll> fact(1,1);
+vector<ll> inv_fact(1,1);
+
+ll choose(int n, int k) {
+    if(k < 0 || k > n) return 0;
+    while(fact.size() < n + 1) {
+        fact.push_back(fact.back() * fact.size() % MOD);
+        inv_fact.push_back(modInverse(fact.back(), MOD));
+    }
+
+    return fact[n] * inv_fact[k] % MOD * inv_fact[n - k] % MOD;
+}
+
 signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cout << fixed << setprecision(15) << '\n';
+    cout << "gimme n :" << flush;
+    ll n;
+    cin >> n;
 
-    vector<ld> dp(100);
-
-    dp[0] = 1;
-    for(int i = 1; i < 100; i++) {
-        dp[i] = .5 * dp[i-1] + (i >= 2 ? dp[i-2] * .5 : 0);
+    ll cnt = 0;
+    for(int i = 1; i <= n; i++) {
+        cnt += choose(n,i) * fact[n-i] * fact[i-1];
+        cout << "i : " << i << " adding : " << choose(n,i) * fact[n-i] * fact[i-1] << '\n';
     }
 
-    for(auto x : dp) {
-        cout << x << '\n';
-    }
+    cout << "Final cnt : " << cnt << '\n';
 
     return 0;
 }
